c21b5cf7da22263d5e4bbf06f1cb4ec2e6a87382
giselopez89@gmail.com <gisellelopez89@hotmail.com>
2014-04-10 01:43:34 -0300

    Integrantes del grupo Codec y primer comit

---
+++ src/integrantesCodec.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : integrantesCodec.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Bienvenidos al grupo Comit y este grupo esta formado por: ");
+	puts("gise lopez ");
+	return EXIT_SUCCESS;
+}
